063a6c708ccba85e011f722e1927b7be73e1ed6b
andrei <andrkool151@gmail.com>
2022-01-30 00:09:35 +0300

    Create Function game_action with all game logic Create print_all_positions function

--- main.py
+++ main.py
@@ -15,12 +15,27 @@ class Action(Enum):
     SHOOT = 102
 
 
+def send_login(game_server, name, password="", game=None, num_turns=None, num_players=1, is_observer=False):
+    data = {"name": name, "password": password, "game": game, "num_turns": num_turns, "num_players": num_players,
+            "is_observer": is_observer}
+
+    login = game_server.send_request(Action.LOGIN, data)
+
+    print("-----LOGIN1: " + str(login))
+    print()
+
+    player_id = login["idx"]
+    return player_id
+
+
 def pick_base_hex(base, vehicles):
     for b in base:
-        for v in vehicles.values():
-            if v["position"] == b:  # hex базы занят
-                break
-        else:
+        # for v in vehicles.values():
+        #     if v["position"] == b:  # hex базы занят
+        #         break
+        #     else:
+        #         return b
+        if hex_is_free(b, vehicles):
             return b
 
 
@@ -31,25 +46,17 @@ def hex_is_free(hex, vehicles):
     return True
 
 
-def main():
-    game_server = ServerHandler.ServerHandler()
+def print_all_positions(game_state):
+    for k, v in game_state["vehicles"].items():
+        vehicle_pos = v["position"]
+        print(k + ": " + str(vehicle_pos))
 
-    data = {"name": "Carlos"}
-    login = game_server.send_request(Action.LOGIN, data)
-    print("-----LOGIN: " + str(login))
-    print()
 
-    game_map = game_server.send_request(Action.MAP)
-    base = game_map['content']['base']
-
-    game_state = game_server.send_request(Action.GAME_STATE)
-    move = {"vehicle_id": 1, "target": {"x": 0, "y": 0, "z": 0}}  # формат хода из доки
-
-    while not game_state['winner']:
-        for k, v in game_state["vehicles"].items():  # ход каждым танком
+def game_action(game_server, player_id, base, game_state, move):
+    for k, v in game_state["vehicles"].items():  # ход каждым танком
+        if v["player_id"] == player_id:  # проверка принадлежнсти танка
             vehicle_pos = v["position"]
             vehicle_hex_pos = Hex(vehicle_pos["x"], vehicle_pos["y"], vehicle_pos["z"])
-
             base_pos = pick_base_hex(base, game_state["vehicles"])  # выбор hex базы для захвата
             if not base_pos:  # вся база занята
                 break
@@ -57,7 +64,6 @@ def main():
 
             # ход в сторону выбранного hex базы с дефолтной скоростью 2 и получение промежуточного hex
             final_hex = vehicle_hex_pos.move_to(base_hex_pos, 2)
-
             if hex_is_free(final_hex.__dict__, game_state["vehicles"]):
                 game_state["vehicles"][k]["position"] = final_hex.__dict__
                 move["vehicle_id"] = k
@@ -66,15 +72,24 @@ def main():
             else:
                 print("---------------HEX IS OCCUPIED!!!----------------")
 
-        game_server.send_request(Action.TURN)
+    game_server.send_request(Action.TURN)
+
+
+def main():
+    game_server = ServerHandler.ServerHandler()
+    player_id = send_login(game_server, name="Carlos")
+
+    game_map = game_server.send_request(Action.MAP)
+    base = game_map['content']['base']
+
+    game_state = game_server.send_request(Action.GAME_STATE)
+    move = {"vehicle_id": 1, "target": {"x": 0, "y": 0, "z": 0}}  # формат хода из доки
+
+    while not game_state['winner']:
+        game_action(game_server, player_id, base, game_state, move)
         game_state = game_server.send_request(Action.GAME_STATE)
 
-    print()
-    print()
-    for k, v in game_state["vehicles"].items():
-        vehicle_pos = v["position"]
-        print(k + ": " + str(vehicle_pos))
-
+    print_all_positions(game_state)
 
 
 if __name__ == "__main__":
