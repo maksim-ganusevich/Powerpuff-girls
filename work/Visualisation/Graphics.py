73a2429356a4a32083574640ba46c5526947845e
andrei <andrkool151@gmail.com>
2022-02-22 01:53:19 +0300

    I tried to make the not vyrviglaznuyu graphics)

--- work/Visualisation/Graphics.py
+++ work/Visualisation/Graphics.py
@@ -6,7 +6,7 @@ from work.config import base, obstacles, global_state
 
 map_size = 11
 
-COLORS = [(255, 153, 0), (204, 51, 204), (102, 153, 255)]
+COLORS = [(46, 196, 182), (231, 29, 54), (255, 159, 28)]
 ICONS_PATH = {
     "at_spg": "work\\Visualisation\\icons\\at_spg.png",
     "heavy_tank": "work\\Visualisation\\icons\\heavy_tank.png",
@@ -86,20 +86,20 @@ class Graphics:
 
     def __draw_empty_map(self):
         r = pygame.Rect(self.__screen_width * 0.57, 0, self.__screen_width * 0.43, self.__screen_height)
-        pygame.draw.rect(self.__screen, (37, 37, 36), r)
+        pygame.draw.rect(self.__screen, (20, 30, 38), r)
         pygame.draw.line(self.__screen, THECOLORS['black'],
                          (self.__screen_width * 0.57, 0),
                          (self.__screen_width * 0.57, self.__screen_height),
                          int(self.__hex_radius))
         for value in self.__hex_cords.values():
             hex_r = self.__screen_height / map_size / 3.5
-            self.__draw_hex((255, 253, 83), 0, hex_r, value)
+            self.__draw_hex((242, 218, 145), 0, hex_r, value)
             self.__draw_hex(THECOLORS['black'], 3, hex_r, value)
 
     def __draw_obstacles(self):
         for obstacle in self.__obstacles:
             position = self.__hex_cords[(obstacle.x, obstacle.y, obstacle.z)]
-            self.__draw_hex((51, 51, 51), 0, self.__hex_radius, position)
+            self.__draw_hex((20, 30, 38), 0, self.__hex_radius, position)
             self.__draw_hex(THECOLORS['black'], 3, self.__hex_radius, position)
 
     def __draw_base(self):
@@ -120,26 +120,25 @@ class Graphics:
             self.__draw_hex(color, 0, self.__hex_radius, position)
             self.__draw_hex(THECOLORS['black'], 3, self.__hex_radius, position)
             tank_surf = pygame.image.load(ICONS_PATH[tank_type])
-            scale = pygame.transform.scale(
+            scale = pygame.transform.smoothscale(
                 tank_surf, (1.5 * self.__hex_radius,
                             1.5 * self.__hex_radius))
             tank_rect = scale.get_rect(center=position)
             self.__screen.blit(scale, tank_rect)
-            hp_str = font_hp.render(str(hp), False,
-                                    THECOLORS["green"])
+            hp_str = font_hp.render(str(hp), True, (127, 255, 0))
             self.__screen.blit(hp_str, (position[0] - self.__hex_radius * 0.7, position[1] - self.__hex_radius * 0.7))
 
     def __draw_turns(self):
-        font = pygame.font.SysFont("calibri", int(self.__font_size * 1.5), True)
+        font = pygame.font.SysFont("calibri", int(self.__font_size * 1.5), False)
         turns_message = "TURN: %s/%s" % (global_state["current_turn"], global_state["num_turns"])
-        text = font.render(turns_message, True, (102, 205, 170))
-        position = self.__screen_width * 21 / 30, self.__screen_height / 2 - self.__hex_radius * 1.5
+        text = font.render(turns_message, True, (242, 153, 75))
+        position = self.__screen_width * 205 / 300, self.__screen_height / 2 - self.__hex_radius * 1.5
         self.__screen.blit(text, position)
 
     def __draw_players(self):
-        font_b = pygame.font.SysFont("calibri", self.__font_size, True)
-        font_l = pygame.font.SysFont("calibri", int(self.__font_size / 1.5), True)
-        text = font_b.render("PLAYERS:", True, (102, 205, 170))
+        font_b = pygame.font.SysFont("calibri", self.__font_size, False)
+        font_l = pygame.font.SysFont("calibri", int(self.__font_size / 1.5), False)
+        text = font_b.render("PLAYERS:", True, (242, 153, 75))
         position = self.__screen_width * 2 / 3, self.__screen_height / 15
         self.__screen.blit(text, position)
         for i, player in enumerate(global_state["players"]):
@@ -149,9 +148,9 @@ class Graphics:
             self.__screen.blit(text, position)
 
     def __draw_win_points(self):
-        font_b = pygame.font.SysFont("calibri", self.__font_size, True)
-        font_l = pygame.font.SysFont("calibri", int(self.__font_size / 1.5), True)
-        text = font_b.render("WIN POINTS:", True, (102, 205, 170))
+        font_b = pygame.font.SysFont("calibri", self.__font_size, False)
+        font_l = pygame.font.SysFont("calibri", int(self.__font_size / 1.5), False)
+        text = font_b.render("WIN POINTS:", True, (242, 153, 75))
         position = self.__screen_width * 2 / 3, self.__screen_height * 3 / 4
         self.__screen.blit(text, position)
         position = self.__screen_width * 1.9 / 3, self.__screen_height * 3 / 4 + self.__hex_radius
@@ -171,7 +170,7 @@ class Graphics:
             winner_id = global_state["winner"]
             rect = pygame.Rect(
                 (self.__screen_width * 0.2, self.__screen_height * 0.3, self.__screen_width * 0.6, self.__screen_height * 0.4))
-            pygame.draw.rect(self.__screen, self.__tank_colors[winner_id], rect, 0)
+            pygame.draw.rect(self.__screen, (119, 148, 166), rect, 0)
             pygame.draw.rect(self.__screen, THECOLORS['black'], rect, 5)
             font = pygame.font.SysFont("calibri", self.__font_size * 2, True)
             win_message = "%s win!" % self.__get_name(winner_id)
@@ -180,7 +179,7 @@ class Graphics:
             self.__screen.blit(text, text_rect)
 
     def __draw_all(self):
-        self.__screen.fill((47, 79, 79))
+        self.__screen.fill((37, 59, 89))
         self.__draw_empty_map()
         self.__draw_base()
         self.__draw_obstacles()
@@ -193,6 +192,9 @@ class Graphics:
 
     def render(self):
         pygame.init()
+        pygame.display.set_caption('World of Tanks: Strategy')
+        pygame.display.set_icon(pygame.image.load("work\\Visualisation\\icons\\powerpuff_girls.jpg"))
+
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
@@ -200,6 +202,3 @@ class Graphics:
                     sys.exit()
             self.__draw_all()
             pygame.display.update()
-
-
-
