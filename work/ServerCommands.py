e6f19337c93e3919917ac2f3718bfcc1903b10d1
slava <slavysia02@gmail.com>
2022-03-09 00:34:47 +0300

    - Action, Result enums instead of ints - server data constants

---
+++ work/ServerCommands.py
@@ -0,0 +1,22 @@
+from enum import Enum
+
+
+class Result(Enum):
+    OKEY = 0
+    BAD_COMMAND = 1
+    ACCESS_DENIED = 2
+    INAPPROPRIATE_GAME_STATE = 3
+    TIMEOUT = 4
+    INTERNAL_SERVER_ERROR = 500
+
+
+class Action(Enum):
+    LOGIN = 1
+    LOGOUT = 2
+    MAP = 3
+    GAME_STATE = 4
+    GAME_ACTIONS = 5
+    TURN = 6
+    CHAT = 100
+    MOVE = 101
+    SHOOT = 102
